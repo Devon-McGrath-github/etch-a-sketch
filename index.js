dd7b16d486836199c7793fcd8ea8e71394d57622
Devon McGrath <devon.mcgrath@gmail.com>
2023-05-11 23:11:26 +1200

    Add mousedown event listener and logic check to changeColor

--- index.js
+++ index.js
@@ -3,17 +3,37 @@
 // make the divs apppear as a grid
 const DEFAULT_SIZE = 16
 
+// creates pixel grid for Etch-A-Sketch
 function createGrid (size) {
   grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
   grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
 
   for (let i = 0; i < size * size; i++) {
     const gridElement = document.createElement('div')
+
     gridElement.classList.add('grid-element')
-    // will have to add mouseover event listener to handle clicking   
+    gridElement.addEventListener("mouseover", changeColor)
+    gridElement.addEventListener("mousedown", changeColor)
     grid.appendChild(gridElement)
   }
 }
+
+function changeColor (e) {
+  if (e.type === 'mouseover' && !mouseDown) return
+  if (mouseDown) {
+    console.log('mousedown true' + mouseDown)
+    // placeholder values for color mode
+    const R = 0
+    const G = 0
+    const B = 0
+    e.target.style.backgroundColor = `rgb(${R}, ${G}, ${B})`
+  }  
+}
+
+// variable to track state for mousedown, that can be checked with mouseover eventListener
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
 
 
 window.onload = () => {
