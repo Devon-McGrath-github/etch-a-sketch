a76059933fc73d5632476e61db7537b0ec925690
Devon McGrath <devon.mcgrath@gmail.com>
2023-05-11 21:00:36 +1200

    Add createGrid Function, fix css reset styling (meyer), index script typo

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 // Create canvas (default to 16x16 grid)
 // Put divs inside another container div
 // make the divs apppear as a grid
-// have no margin
+const DEFAULT_SIZE = 16
 
-const div = document.createElement('div')
+console.log('linked stylesheet')
 
+function createGrid (size) {
+
+  for (let i = 0; i < size * size; i++) {
+    const gridElement = document.createElement('div')
+    gridElement.classList.add('grid-element')
+
+    grid.appendChild(gridElement)
+  }
+}
+
+
+window.onload = () => {
+  createGrid(DEFAULT_SIZE)
+}
