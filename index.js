5fafc9180981ad196d19feb05a3aa72f0229cd63
Devon McGrath <devon.mcgrath@gmail.com>
2023-05-11 22:51:26 +1200

    Add mouseover coloring gridElements

--- index.js
+++ index.js
@@ -4,11 +4,13 @@
 const DEFAULT_SIZE = 16
 
 function createGrid (size) {
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
 
   for (let i = 0; i < size * size; i++) {
     const gridElement = document.createElement('div')
     gridElement.classList.add('grid-element')
-
+    // will have to add mouseover event listener to handle clicking   
     grid.appendChild(gridElement)
   }
 }
