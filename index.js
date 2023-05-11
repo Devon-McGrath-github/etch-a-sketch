7ff7ad47eff8fc4b055b0591e20c5f78a086d7b0
Devon McGrath <devon.mcgrath@gmail.com>
2023-05-11 20:39:24 +1200

    Meyer Reset

---
+++ index.js
@@ -0,0 +1,7 @@
+// Create canvas (default to 16x16 grid)
+// Put divs inside another container div
+// make the divs apppear as a grid
+// have no margin
+
+const div = document.createElement('div')
+
